dbcf94cf258cc0e5440b0244aacad9db8a488cc8
becca-schraibman <55113111+becca-schraibman@users.noreply.github.com>
2020-03-29 22:32:17 -0400

    Add files via upload

--- mugen-distribution-master 2/demo/script.js
+++ mugen-distribution-master 2/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 2500, npcCollisionRadius: 100, actionDuration:700, jumpHeight: 200, heroSpeed: 200});
+  mugen.initialize({titleScreenImageURL: "title-screen.png", actionRadius: 100, timeBetweenNPCs: 2500, npcCollisionRadius: 100, actionDuration:700, jumpHeight: 200, heroSpeed: 200});
 });
